46187019e69bca3c6a8483798ce383576f45cd0a
lulmer-personal-development <ulmer22.lucas@gmail.com>
2021-11-27 22:55:12 -0600

    Created EditDistance function to compute minimum number of operations to convert one string to another

---
+++ EditDistance/edit_distance.go
@@ -0,0 +1,81 @@
+package dandistance
+
+import (
+	"fmt"
+)
+
+const MAX_STRING_LENGTH = 500
+
+func EditDistance(a, b string) (int, error) {
+	aLength := len(a)
+	bLength := len(b)
+
+	err := validateStrings(a, b, aLength, bLength)
+	if err != nil {
+		return -1, err
+	}
+
+	if aLength == 0 {
+		return bLength, nil
+	}
+	if bLength == 0 {
+		return aLength, nil
+	}
+
+	convertMatrix := make([][]int, aLength+1)
+	for i := range convertMatrix {
+		convertMatrix[i] = make([]int, bLength+1)
+	}
+
+	for i := 0; i <= aLength; i++ {
+		convertMatrix[i][0] = i
+	}
+	for j := 0; j <= bLength; j++ {
+		convertMatrix[0][j] = j
+	}
+
+	for i := 1; i <= aLength; i++ {
+		for j := 1; j <= bLength; j++ {
+			if a[i-1] == b[j-1] {
+				convertMatrix[i][j] = convertMatrix[i-1][j-1]
+			} else {
+				convertMatrix[i][j] = min(convertMatrix[i-1][j-1], convertMatrix[i-1][j], convertMatrix[i][j-1]) + 1
+			}
+		}
+	}
+
+	return convertMatrix[aLength][bLength], nil
+}
+
+func validateStrings(a, b string, aLength, bLength int) error {
+	if aLength > MAX_STRING_LENGTH || bLength > MAX_STRING_LENGTH {
+		return fmt.Errorf("input string exceeds the maximum length of %d", MAX_STRING_LENGTH)
+	}
+
+	for _, aChar := range a {
+		if !isLowerCaseEnglishChar(aChar) {
+			return fmt.Errorf("string a contains a character that is not lower case English")
+		}
+	}
+	for _, bChar := range b {
+		if !isLowerCaseEnglishChar(bChar) {
+			return fmt.Errorf("string b contains a character that is not lower case English")
+		}
+
+	}
+	return nil
+}
+
+func isLowerCaseEnglishChar(char rune) bool {
+	return (char >= 'a' && char <= 'z')
+}
+
+func min(a, b, c int) int {
+	if a <= b && a <= c {
+		return a
+	} else if b <= a && b <= c {
+		return b
+	} else {
+		return c
+	}
+}
---
+++ EditDistance/edit_distance_test.go
@@ -0,0 +1,82 @@
+package dandistance
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_EditDistanceSuccesses(t *testing.T) {
+	cases := []struct {
+		name                string
+		word1               string
+		word2               string
+		expectedDistance    int
+		expectedErrorString string
+	}{
+		{
+			name:             "horse to ros",
+			word1:            "horse",
+			word2:            "ros",
+			expectedDistance: 3,
+		},
+		{
+			name:             "intention to execution",
+			word1:            "intention",
+			word2:            "execution",
+			expectedDistance: 5,
+		},
+		{
+			name:             "abcd to acdb",
+			word1:            "abcd",
+			word2:            "acdb",
+			expectedDistance: 2,
+		},
+		{
+			name:             "word1 is empty string",
+			word1:            "",
+			word2:            "converted",
+			expectedDistance: 9,
+		},
+		{
+			name:             "word2 is empty string",
+			word1:            "string",
+			word2:            "",
+			expectedDistance: 6,
+		},
+		{
+			name:                "string has capitals",
+			word1:               "CAPITALS",
+			word2:               "string",
+			expectedDistance:    -1,
+			expectedErrorString: "string a contains a character that is not lower case English",
+		},
+		{
+			name:                "string has numeric characters",
+			word1:               "string",
+			word2:               "1234",
+			expectedDistance:    -1,
+			expectedErrorString: "string b contains a character that is not lower case English",
+		},
+		{
+			name:                "string has special characters",
+			word1:               "!@#$%",
+			word2:               "string",
+			expectedDistance:    -1,
+			expectedErrorString: "string a contains a character that is not lower case English",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual, err := EditDistance(c.word1, c.word2)
+
+			assert.Equal(t, c.expectedDistance, actual)
+			if err != nil {
+				assert.EqualError(t, err, c.expectedErrorString)
+			} else {
+				assert.Nil(t, err)
+			}
+		})
+	}
+}
